6f4a894c0c0d9512add3e94d2ba798a80ba721d1
Roque Armando Ramírez <109245164+Armando8bits@users.noreply.github.com>
2024-06-22 18:29:54 -0500

    avances en la automatización, falta mejorar saltar la deteccion de selenium

--- src/Acciones/plataforma.py
+++ src/Acciones/plataforma.py
@@ -9,24 +9,38 @@ from selenium.webdriver.support import expected_conditions as EC #necesario para
 from selenium.webdriver.common.by import By #necesario para el de arriba
 
 import time
+import random
 
 class udemy:
     def ProcesarCursos(df_cursos, usuario, contras):
         print("> ACCEDIENDO A SITIO WEB...")
         options = Options()
-        options.add_argument("--incognito")
-        # Adding argument to disable the AutomationControlled flag 
-        options.add_argument("--disable-blink-features=AutomationControlled") 
-        # Exclude the collection of enable-automation switches 
-        options.add_experimental_option("excludeSwitches", ["enable-automation"]) 
-        # Turn-off userAutomationExtension 
-        options.add_experimental_option("useAutomationExtension", False) 
+        #options.add_argument("--incognito")
+        options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36")
+
+        # Deshabilitar la automatización de Chrome
+        options.add_experimental_option("useAutomationExtension", False)
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        # Deshabilitar WebRTC
+        options.add_argument("--disable-webrtc")
+
+        # Deshabilitar WebGL
+        options.add_argument("--disable-webgl")
+
+        # Usar un perfil de navegador real
+        options.add_argument("user-data-dir=/path/to/your/custom/profile")
+
+        # Configuración para evitar la detección
+        options.add_argument("--disable-blink-features=AutomationControlled")
+
         '''El perfil de Chrome "Incognito" no verifica los certificados SSL por defecto. Esta opción es más segura que la anterior,
     ya que el perfil de incognito no guarda cookies ni historial de navegación. en lugar de usar el argumento "--ignore-certificate-errors"
     al inicio de Chrome, lo que le indicará que ignore cualquier error relacionado con los certificados SSL. pero este último no es recomendado
     ya que una práctica insegura y no se recomienda, ya que expone su conexión a posibles ataques y robo de datos. '''     
         
         driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+        # Modificar la propiedad de WebDriver en tiempo de ejecución
+        driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
         driver.maximize_window() #creo que es obvio lo que hace
         driver.get("https://www.udemy.com/")
 
@@ -42,8 +56,18 @@ class udemy:
         return
     
     def iniciarSesion(driver,usuari,passwd):
+        human_like_mouse_movements(driver)
         time.sleep(4.1)
         WebDriverWait(driver,30)\
             .until(EC.element_to_be_clickable((By.XPATH,"//span[contains(.,'Iniciar sesión')]")))\
             .click()
+        #investigar si me detecta al hacer este click investigar otra forma
         return True
+    
+    # Simular comportamiento humano
+def human_like_mouse_movements(driver):
+    for _ in range(10):
+        x = random.randint(0, 100)
+        y = random.randint(0, 100)
+        driver.execute_script(f"window.scrollBy({x}, {y});")
+        time.sleep(random.uniform(0.5, 1.5))
